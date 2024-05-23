7c1e6af9e27ce00e274d9917ce5aa1d592a5208f
haq-intekhab <145752824+haq-intekhab@users.noreply.github.com>
2024-05-23 17:41:33 +0530

    Update Settlements.jsx

--- src/component/Settlements.jsx
+++ src/component/Settlements.jsx
@@ -61,10 +61,10 @@ const Settlements = () => {
 
           <div className="w-full h-fit flex flex-col justify-center items-center md:mt-8 mt-0">
             {data1?.map((data, index) => (
-              <div className="w-[95%]">
+              <div className="w-full">
                 <div
                   key={index}
-                  className="w-full h-[4rem] flex flex-row md:gap-14 gap-8 border-2 rounded-2xl md:p-6 p-2 mt-4 relative"
+                  className="w-full h-[4rem] flex flex-row md:gap-14 gap-8 border-2 rounded-2xl md:p-6 items-center p-4 mt-4 relative"
                 >
                   <div className="md:w-[38%] w-[25%] flex md:flex-row flex-col  justify-between items-center">
                     <p className="font-semibold text-black text-base md:ml-8 ml-2">
@@ -128,9 +128,9 @@ const Settlements = () => {
 
                 {showTransDetails === data.id && (
                   <div className=" h-fit flex md:flex-row flex-col gap-4  md:mt-8 mt-4 w-full border-2 rounded-2xl md:p-4 p-2 ">
-                    <div className="md:w-[38%] w-full flex md:flex-row flex-col gap-6 justify-between md:border-0 border-2 rounded-lg">
-                      <div className="md:w-[48%] w-full flex md:justify-between justify-evenly">
-                        <div className="flex flex-col justify-evenly">
+                    <div className="md:w-[38%] w-full flex md:flex-row flex-col gap-2 justify-between md:border-0 border-2 rounded-lg">
+                      <div className="md:w-[52%] w-full flex md:gap-4 gap-4 sm:ml-0 ml-8">
+                        <div className="flex flex-col gap-10 md:mt-6 mt-3">
                           {steps?.map((step, i) => (
                             <div
                               key={i}
@@ -148,20 +148,20 @@ const Settlements = () => {
                             </div>
                           ))}
                         </div>
-                        <div className="flex flex-col md:gap-6 gap-2 md:mt-7 mt-3">
-                          <p className="text-sm">Payout Request Created</p>
-                          <p className="text-sm">Payment Settled</p>
+                        <div className="flex flex-col md:gap-9 gap-8 md:mt-6 mt-3">
+                          <p className="text-nowrap text-[12px]">Payout Request Created</p>
+                          <p className="text-nowrap text-[12px]">Payment Settled</p>
                         </div>
                       </div>
                       <p className="border md:hidden block"></p>
                       <div className="md:w-[47%] w-full flex flex-col gap-1 justify-center text-center">
-                        <p className="font-semibold text-2xl text-center">
+                        <p className="font-semibold text-[24px] text-center">
                           ₹ {data.Amount}
                         </p>
-                        <p className="w-full font-semibold md:text-sm text-lg">
+                        <p className="w-full font-semibold text-nowrap text-[14px]">
                           Settlement from Snackbae
                         </p>
-                        <p className="text-slate-400 text-sm text-center mb-2">
+                        <p className="text-slate-400 text-nowrap text-[14px] text-center mb-2">
                           Saturday, Apr 15, 2021
                         </p>
                         {/* <p className="font-semibold text-xl mt-4 text-center mb-4">
@@ -169,40 +169,40 @@ const Settlements = () => {
                         </p> */}
                       </div>
                     </div>
-                    <div className="md:w-[58%] w-full flex md:flex-row flex-col ">
-                      <div className="md:w-[40%] w-full flex flex-col gap-4 md:border-0 border-2 rounded-lg md:mt-0 mt-4">
-                        <p className="w-full font-semibold text-center text-black border-b-2 pb-2">
+                    <div className="md:w-[60%] w-full flex md:flex-row flex-col ">
+                      <div className="md:w-[38%] w-full flex flex-col gap-4 md:border-0 border-2 rounded-lg md:mt-0 mt-4">
+                        <p className="w-full font-semibold text-center text-black text-[14px] border-b-2 pb-2">
                           Payout Account
                         </p>
                         <div className="flex gap-6 justify-between px-2">
-                          <p className=" text-slate-400">UPI ID</p>
-                          <p className="font-semibold ">ABCDEFG@YBL</p>
+                          <p className=" text-slate-400 text-nowrap text-[14px]">UPI ID</p>
+                          <p className="font-semibold text-[14px]">ABCDEFG@YBL</p>
                         </div>
                         <div className="flex justify-between px-2">
-                          <p className=" text-slate-400">UPI Number </p>
-                          <p className="font-semibold mb-2">+9191234567</p>
+                          <p className=" text-slate-400 text-nowrap text-[14px]">UPI Number </p>
+                          <p className="font-semibold mb-2 text-[14px]">+9191234567</p>
                         </div>
                       </div>
-                      <div className="md:w-[40%] w-full flex flex-col gap-4 md:border-0 border-2 md:ml-4 rounded-lg md:mt-0 mt-4">
-                        <p className="font-semibold text-center text-black border-b-2 pb-2">
+                      <div className="md:w-[38%] w-full flex flex-col gap-4 md:border-0 border-2 md:ml-4 rounded-lg md:mt-0 mt-4">
+                        <p className="font-semibold text-center text-[14px] text-black border-b-2 pb-2">
                           Settlement Breakup
                         </p>
                         <div className="flex gap-6 px-2">
-                          <p className="text-slate-400">Total Collection </p>
-                          <p className="font-semibold ">{data.Amount}</p>
+                          <p className="text-slate-400 text-nowrap text-[14px]" >Total Collection </p>
+                          <p className="font-semibold text-[14px]">{data.Amount}</p>
                         </div>
                         <div className="flex gap-6 px-2">
-                          <p className="text-slate-400">Gateway Fee(2%)</p>
-                          <p className="font-semibold text-blue-500 mb-2">
+                          <p className="text-slate-400 text-nowrap text-[14px]">Gateway Fee(2%)</p>
+                          <p className="font-semibold text-blue-500 mb-2 text-[14px]">
                             100
                           </p>
                         </div>
                       </div>
                       <div className="md:w-[20%] w-full flex flex-col gap-4 md:border-0 border-2 rounded-lg md:mt-0 mt-4">
-                        <p className="w-full font-semibold text-center text-nowrap text-slate-400 border-b-2 pb-2">
+                        <p className="w-full font-semibold  text-nowrap text-[14px] text-slate-400 border-b-2 pb-2">
                           Amount you get
                         </p>
-                        <p className="text-center font-semibold text-xl md:mt-6 mb-2">
+                        <p className="text-center font-semibold text-[24px] md:mt-6 mb-2">
                           4900
                         </p>
                       </div>
