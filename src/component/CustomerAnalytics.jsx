d19bd3db60509739ae9eb98f3af75ef9d5d1a681
Nitish kumar <139990315+Nitish0604@users.noreply.github.com>
2024-06-11 20:59:55 +0530

    Update CustomerAnalytics.jsx

--- src/component/CustomerAnalytics.jsx
+++ src/component/CustomerAnalytics.jsx
@@ -20,7 +20,7 @@ const CustomerAnalytics = () => {
     const [birthdayCount, setBirthdayCount] = useState(0);
     const [maleCount, setMaleCount] = useState(0);
     const [femaleCount, setFemaleCount] = useState(0);
-    const [lowRecommendation,setLowRecommendation] = useState(0);
+    const [lowRecommendation, setLowRecommendation] = useState(0);
 
 
     const getRestaurantData = async (req, res) => {
@@ -123,140 +123,160 @@ const CustomerAnalytics = () => {
         <div className='w-full bg-[#F6F8FF] sm:px-6 h-fit  '>
 
             <div className='flex flex-col w-full h-fit   mt-6'>
-                <div className='flex gap-4 w-full h-fit  my-9 sm:flex-row flex-col '>
+                <div className='flex gap-3 w-full   my-2 sm:flex-row flex-col '>
 
-
-                    <div className='sm:w-[32%] min-h-[200px] border border-[#00000080] rounded-lg bg-white '>
-                        <div className='flex gap-4 flex-col  w-full h-[75%] p-[1rem] border-b border-[#00000080]'>
-                            <div className='text-[1.3rem] text-[#777980]'>
+                    <div className='sm:w-[32%] h-[140px]  border border-[#00000080] rounded-lg bg-white'>
+                        <div className='flex gap-4 flex-col h-full  w-full  p-[1rem]  border-[#00000080]'>
+                            <div className='text-[1.1rem] text-[#777980]'>
                                 <p>Total Customers Visited in</p>
                                 <p>Last 15 Days</p>
                             </div>
-                            <p className='text-[1.8rem] text-[#1D1F2C]'>{last15Days}</p>
+                            <p className='text-[1.5rem] text-[#1D1F2C] font-medium'>{last15Days}</p>
                         </div>
-                        <div className='w-full flex items-center justify-evenly text-[1.6rem] h-[25%] py-[.5rem]'>
-
-                            <MdForwardToInbox className='text-[#004AAD] ' />
-                            <FiPhone className='text-[#004AAD]' />
-                            <IoLogoWhatsapp className='text-green-400' />
-
-                            <button className='bg-[#004AAD] text-white py-2 sm:px-3 px-2 rounded-md text-[1rem] mr-2'>
-                                Send Campaign</button>
-                        </div>
-
-
-
                     </div>
 
-                    <div className='sm:w-[32%] min-h-[200px] border border-[#00000080] rounded-lg bg-white '>
-                        <div className='flex gap-4 flex-col  w-full h-[75%] p-[1rem] border-b border-[#00000080]'>
-                            <div className='text-[1.3rem] text-[#777980]'>
+                    <div className='sm:w-[32%] h-[140px] border border-[#00000080] rounded-lg bg-white '>
+                        <div className='flex gap-4  flex-col  w-full  p-[1rem] '>
+                            <div className='text-[1.1rem] text-[#777980]'>
                                 <p>Total Customers Visited in</p>
                                 <p>Last 30 Days</p>
                             </div>
-                            <p className='text-[1.8rem] text-[#1D1F2C]'>{last30Days}</p>
+                            <p className='text-[1.5rem] text-[#1D1F2C] font-medium'>{last30Days}</p>
                         </div>
-                        <div className='w-full flex items-center justify-evenly text-[1.6rem] h-[25%] py-[.5rem]'>
-
-                            <MdForwardToInbox className='text-[#004AAD] ' />
-                            <FiPhone className='text-[#004AAD]' />
-                            <IoLogoWhatsapp className='text-green-400' />
-
-                            <button className='bg-[#004AAD] text-white py-2 sm:px-3 px-2 rounded-md text-[1rem] mr-2'>
-                                Send Campaign</button>
-                        </div>
-
-
 
                     </div>
 
-                    <div className='sm:w-[32%] min-h-[200px] border border-[#00000080] rounded-lg bg-white '>
-                        <div className='flex gap-4 flex-col  w-full h-[75%] p-[1rem] border-b border-[#00000080]'>
-                            <div className='text-[1.3rem] text-[#777980]'>
-                                <p>Customer Birthday this</p>
-                                <p>month</p>
-                            </div>
-                            <p className='text-[1.8rem] text-[#1D1F2C]'>{birthdayCount}</p>
-                        </div>
-                        <div className='w-full flex items-center justify-evenly text-[1.6rem] h-[25%] py-[.5rem]'>
-
-                            <MdForwardToInbox className='text-[#004AAD] ' />
-                            <FiPhone className='text-[#004AAD]' />
-                            <IoLogoWhatsapp className='text-green-400' />
-
-                            <button className='bg-[#004AAD] text-white py-2 sm:px-3 px-2 rounded-md text-[1rem] mr-2'>
-                                Send Campaign</button>
-                        </div>
-
-
-
-                    </div>
-                </div>
-
-                {/* Customers at Risk */}
-
-                <div className='w-full my-3 font-Roboto text-[#000000]'>
-                    <p className=' text-[1.9rem] font-bold '>Customers at Risk</p>
-                    <p className='text-[.9rem]'>Utilize customer analytics to make well-informed decisions.</p>
-                </div>
-
-                <div className='flex gap-4 w-full  h-fit my-9 sm:flex-row flex-col relative '>
-
-                    <div className='sm:w-[32%] min-h-[200px] border border-[#00000080] rounded-lg bg-white '>
-                        <div className='flex gap-4 flex-col  w-full h-[75%] p-[1rem] border-b border-[#00000080]'>
-                            <div className='text-[1.1rem] text-[#004AAD]'>
+                    <div className='sm:w-[32%] h-[140px] border border-[#00000080] rounded-lg bg-white '>
+                        <div className='flex gap-4 flex-col  w-full  p-[1rem] '>
+                            <div className='text-[1.1rem] text-[#777980]'>
                                 <p>Total Customers has not</p>
                                 <div className='flex gap-2'>
                                     <p>Visited in Last</p>
-                                    <p className='text-[#F44336]'>30 Days</p>
+                                    <p className='text-[#F44336]'>60 Days</p>
                                 </div>
 
                             </div>
-                            <p className='text-[1.8rem] text-[#1D1F2C]'>{totalCustomers - last30Days}</p>
+                            <p className='text-[1.5rem] text-[#1D1F2C] font-medium'>{totalCustomers - last30Days}</p>
                         </div>
-                        <div className='w-full flex items-center justify-evenly text-[1.6rem] h-[25%] py-[.5rem] '>
-
-                            <MdForwardToInbox className='text-[#004AAD] ' />
-                            <FiPhone className='text-[#004AAD]' />
-                            <IoLogoWhatsapp className='text-green-400' />
-
-                            <button className='bg-[#004AAD] text-white py-2 sm:px-3 px-2 rounded-md text-[1rem] mr-2'>
-                                Send Campaign</button>
-                        </div>
-
-
 
                     </div>
-                    <div className='sm:w-[32%] min-h-[200px] border border-[#00000080] rounded-lg bg-white '>
-                        <div className='flex gap-4 flex-col  w-full h-[75%] p-[1rem] border-b border-[#00000080]'>
+                </div>
+
+                <div className='flex gap-3 w-full   my-2 sm:flex-row flex-col '>
+
+                    <div className='sm:w-[32%] h-[140px]  border border-[#00000080] rounded-lg bg-white'>
+                        <div className='flex gap-4 flex-col   w-full  p-[1rem]  '>
                             <div className='text-[1.1rem] text-[#004AAD]'>
-                                <p>Total Customers with low</p>
-                                <div className='flex gap-2'>
-                                    <p>Recommendation in last</p>
-                                    <p className='text-[#F44336]'> 30 Days</p>
-                                </div>
+                                <p>Returning customer in last </p>
+                                <p className='text-[#10B981]'> 60 Days</p>
                             </div>
-                            <p className='text-[1.8rem] text-[#1D1F2C]'>{lowRecommendation}</p>
+                            <p className='text-[1.5rem] text-[#1D1F2C] font-medium'>{last15Days}</p>
                         </div>
-                        <div className='w-full flex items-center justify-evenly text-[1.6rem] h-[25%] py-[.5rem]'>
+                        
 
-                            <MdForwardToInbox className='text-[#004AAD] ' />
-                            <FiPhone className='text-[#004AAD]' />
-                            <IoLogoWhatsapp className='text-green-400' />
+                    </div>
 
-                            <button className='bg-[#004AAD] text-white py-2 sm:px-3 px-2 rounded-md text-[1rem] mr-2'>
-                                Send Campaign</button>
+                    <div className='sm:w-[32%] h-[140px] border border-[#00000080] rounded-lg bg-white '>
+                        <div className='flex gap-4 flex-col  w-full  p-[1rem]  '>
+                            <div className='text-[1.1rem] text-[#777980]'>
+                                <p>Customer Birthday this</p>
+                                <p>month</p>
+                            </div>
+                            <p className='text-[1.5rem] text-[#1D1F2C] font-medium'>{birthdayCount}</p>
                         </div>
+                        
 
 
 
                     </div>
+
+                    <div className='sm:w-[32%] h-[140px] border border-[#00000080] rounded-lg bg-white '>
+                        <div className='flex gap-4 flex-col  w-full  p-[1rem]  '>
+                            <div className='text-[1.1rem] text-[#777980]'>
+                                <p>Customer Anniversary this</p>
+                                <p>month</p>
+                            </div>
+                            <p className='text-[1.5rem] text-[#1D1F2C] font-medium'>{birthdayCount}</p>
+                        </div>
+                      
+
+
+
+                    </div>
+                </div>
+
+                <div className='flex gap-3 w-full  h-fit my-2 sm:flex-row flex-col   '>
+
+                    <div className='sm:w-[66%] w-full flex h-fit gap-3 flex-wrap justify-between'>
+                        <div className='sm:w-[49%] w-full h-[140px]  border border-[#00000080] rounded-lg bg-white '>
+                            <div className='flex gap-4 flex-col  w-full  p-[1rem] '>
+                                <div className='text-[1.1rem] text-[#004AAD]'>
+                                    <p>Customer participated in </p>
+                                    <div className='flex gap-2'>
+                                        <p>games in last</p>
+                                        <p className='text-[#10B981]'>60 Days</p>
+                                    </div>
+
+                                </div>
+                                <p className='text-[1.5rem] text-[#1D1F2C] font-medium'>2,412</p>
+                            </div>
+                           
+
+                        </div>
+                        <div className='sm:w-[49%] w-full h-[140px]  border border-[#00000080] rounded-lg bg-white '>
+                            <div className='flex gap-4 flex-col  w-full  p-[1rem] '>
+                                <div className='text-[1.1rem] text-[#004AAD]'>
+                                    <p>Customer won games in </p>
+                                    <div className='flex gap-2'>
+                                        <p>last</p>
+                                        <p className='text-[#10B981]'>60 Days</p>
+                                    </div>
+
+                                </div>
+                                <p className='text-[1.5rem] text-[#1D1F2C] font-medium'>1412</p>
+                            </div>
+                           
+
+                        </div>
+                        <div className='sm:w-[49%] w-full h-[140px]  border border-[#00000080] rounded-lg bg-white '>
+                            <div className='flex gap-4 flex-col  w-full  p-[1rem] '>
+                                <div className='text-[1.1rem] text-[#004AAD]'>
+                                    <p>Total Customers with positive  </p>
+                                    <div className='flex gap-2'>
+                                        <p>recommendation</p>
+                                        <p className='text-[#F44336]'></p>
+                                    </div>
+
+                                </div>
+                                <p className='text-[1.5rem] text-[#1D1F2C] font-medium'>6,999</p>
+                            </div>
+                           
+
+                        </div>
+                        <div className='sm:w-[49%] w-full h-[140px]  border border-[#00000080] rounded-lg bg-white '>
+                            <div className='flex gap-4 flex-col  w-full  p-[1rem] '>
+                                <div className='text-[1.1rem] text-[#004AAD]'>
+                                    <p>Total Customers with positive </p>
+                                    <div className='flex gap-2'>
+                                        <p>recommendation</p>
+                                        <p className='text-[#F44336]'></p>
+                                    </div>
+
+                                </div>
+                                <p className='text-[1.5rem] text-[#1D1F2C] font-medium'>1,684</p>
+                            </div>
+                           
+
+                        </div>
+                    </div>
+
+
 
                     {/* charts */}
-                    <div className='sm:w-[32%] min-h-[300px] max-h-[400px] border border-[#00000080] rounded-lg bg-white overflow-hidden sm:absolute right-2 sm:-mt-24  '>
+                    <div className='sm:w-[33%] w-full h-[290px]  border border-[#00000080] rounded-lg bg-white overflow-hidden     '>
                         <div className='flex  flex-col  w-full h-[100%] p-[1rem]  border-[#00000080] relative '>
-                            <p className='text-[#0F172A] font-bold'>Customer Gender segment</p>
-                            <div className='absolute -ml-9 mt-6  border-t  '>
+                            <p className='text-[#0F172A] font-bold absolute border-b border-[#2019ec80] pb-1.5 z-30'>Customer Gender segment</p>
+                            <div className='absolute -ml-[85px] -mt-9  border-t  '>
                                 <Charts male={maleCount} female={femaleCount} />
                             </div>
 
@@ -278,7 +298,7 @@ const CustomerAnalytics = () => {
 
                     </div>
                 </div>
-                <div />
+
 
             </div>
         </div>
