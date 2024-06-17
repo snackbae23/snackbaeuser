d7d3fc2e3236e1736b96863b84a3b4d100921016
Tanmay <121565463+tanmay2809@users.noreply.github.com>
2024-06-17 11:40:25 +0530

    Update CustomerRecords.jsx

--- src/component/CustomerRecords.jsx
+++ src/component/CustomerRecords.jsx
@@ -24,7 +24,7 @@ const CustomerRecords = () => {
     let config = {
       method: "get",
       maxBodyLength: Infinity,
-      url: `https://seashell-app-lgwmg.ondigitalocean.app/api/getRestaurantDetails/${resId}`,
+      url: `https://goldfish-app-yhaxv.ondigitalocean.app/api/getRestaurantDetails/${resId}`,
       // url: `http://localhost:4000/api/getRestaurantDetails/${resId}`,
       headers: {},
     };
